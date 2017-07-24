03121a0f6c84ad7833e78cb8dcd0ec3d9aef30d2
Oriol Ferrer Mesia <oriol@uri.cat>
2017-07-23 21:08:40 -0400

    move to glwf window for example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,16 +1,11 @@
-#include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
-//========================================================================
-int main( ){
-
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 800, 600, OF_WINDOW);			// <-------- setup the GL context
-
+int main(){
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofRunApp( new ofApp());
 
+	ofSetupOpenGL(1024, 768, OF_WINDOW);
+
+	ofRunApp(new ofApp());
 }
